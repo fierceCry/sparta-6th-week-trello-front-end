75efbd008e8c2f1a3a295e7c99b597c495b60e22
leegilhyeon <164996803+leegilhyeon@users.noreply.github.com>
2024-06-06 15:56:37 +0900

    페이지네이션 css

--- src/post/MainPage.jsx
+++ src/post/MainPage.jsx
@@ -206,176 +206,220 @@ const MainPage = () => {
   };
 
 
-  return (<div className="main-page-container">
-  <header className="main-header">
-    <div className="links">
-      <img id="logo" src={goodplace} alt="logo" />
-      <Link to="/mypage">
-        <img id="user-icon" src={mypage} alt="user" />
-      </Link>
-      <button id="logout-icon-btn" onClick={handleLogout}>
-        <img id="logout-icon" src={logout} alt="logout" />
-      </button>
-    </div>
-  </header>
-  <h1 id="site-title-first" className="site-title">뭐 먹고싶어</h1>
-  <h1 id="site-title-second" className="site-title">골라</h1>
-  <Link src="../img/Eat.jpg"></Link>
-  <div id='main-middle-aboutpost'>
-    <button className="common-button" onClick={handleCreateFormToggle}>글쓰기</button>
-    <h2>지역별로 고르기</h2>
-    <div className="category-buttons">
-      <button className="region-button" onClick={() => handleCategoryFilter('수도권')}>수도권</button>
-      <button className="region-button" onClick={() => handleCategoryFilter('충청권')}>충청권</button>
-      <button className="region-button" onClick={() => handleCategoryFilter('호남권')}>호남권</button>
-      <button className="region-button" onClick={() => handleCategoryFilter('영남권')}>영남권</button>
-      <button className="region-button" onClick={() => handleCategoryFilter('강원권')}>강원권</button>
-      <button className="region-button" onClick={() => handleCategoryFilter('제주권')}>제주권</button>
-    </div>
-    <button className="common-button" onClick={handleSortToggle}>
-      {sortOrder === 'desc' ? '오래된순' : '최신순'}
-    </button>
-  </div>
-  {/* 페이지 네이션 */}
-  <div className="pagination">
-  {Array.from({ length: totalPages }, (_, index) => {
-    if (totalPages <= 5) {
-      return (
-        <button
-          key={index + 1}
-          onClick={() => handlePageChange(index + 1)}
-          disabled={index + 1 === currentPage}
-        >
-          {index + 1}
-        </button>
-      );
-    } else {
-      if (currentPage <= 3) {
-        // 처음 5페이지 보여주기
-        if (index < 5) {
-          return (
-            <button
-              key={index + 1}
-              onClick={() => handlePageChange(index + 1)}
-              disabled={index + 1 === currentPage}
-            >
-              {index + 1}
-            </button>
-          );
-        } else if (index === 5) {
-          // ... 표시
-          return <span key={index}>...</span>;
-        }
-      } else if (currentPage > totalPages - 5) {
-        // 마지막 5페이지 보여주기
-        if (index >= totalPages - 2) {
-          return (
-            <button
-              key={index + 1}
-              onClick={() => handlePageChange(index + 1)}
-              disabled={index + 1 === currentPage}
-            >
-              {index + 1}
-            </button>
-          );
-        } else if (index === totalPages - 6) {
-          // ... 표시
-          return <span key={index}>...</span>;
-        }
-      } else {
-        // 현재 페이지와 주변 페이지 보여주기
-        if (index === currentPage - 1 || index === currentPage || index === currentPage + 1) {
-          return (
-            <button
-              key={index + 1}
-              onClick={() => handlePageChange(index + 1)}
-              disabled={index + 1 === currentPage}
-            >
-              {index + 1}
-            </button>
-          );
-        } else if (index === currentPage - 2 || index === currentPage + 2) {
-          // ... 표시
-          return <span key={index}>...</span>;
-        }
-      }
-    }
-    return null;
-  })}
-</div>
-
-  <div id="posts-grid-mom">
-    <ul className="posts-grid">
-      {allPosts.map((post) => (
-        <li className="post-card" key={post.postId}>
-          <Link to={`/post/${post.postId}`} className="post-link">
-            {post.imageUrl && (
-              <img
-                src={
-                  Array.isArray(post.imageUrl)
-                    ? post.imageUrl[0]
-                    : post.imageUrl
-                }
-                alt={post.title}
-                className="post-image"
-              />
-            )}
-            <h3>{post.title}</h3>
-            <small>{post.nickname}</small>
+  return (
+    <div className="main-page-container">
+      <header className="main-header">
+        <div className="links">
+          <img id="logo" src={goodplace} alt="logo" />
+          <Link to="/mypage">
+            <img id="user-icon" src={mypage} alt="user" />
           </Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-  {showCreateForm && (
-    <div className="modal">
-      <div className="modal-content">
-        <span className="close" onClick={handleCreateFormToggle}>
-          &times;
-        </span>
-        <h2 className="modal-header">맛집 추천하기:냠냠:</h2>
-        <input className="modal-title"
-          type="text"
-          name="title"
-          placeholder="제목"
-          value={newPost.title}
-          onChange={handleInputChange}
-        />
-        <textarea className="modal-content-input"
-          type="text"
-          name="content"
-          placeholder="내용을 작성해주세요"
-          value={newPost.content}
-          onChange={handleInputChange}
-        ></textarea>
-        <input
-          type="file"
-          name="imageUrl"
-          placeholder="Image URL"
-          // value={newPost.imageUrl}
-          onChange={handleImageInputChange}
-          multiple  // 다중 파일 선택을 지원하도록 multiple 속성을 추가합니다.
-        />
-        <select
-          name="category"
-          value={newPost.regionId}
-          onChange={handleCategoryChange}
-        >
-          <option value="1">수도권</option>
-          <option value="2">충청권</option>
-          <option value="3">호남권</option>
-          <option value="4">영남권</option>
-          <option value="5">강원권</option>
-          <option value="6">제주권</option>
-        </select>
-        <button className="modal-btn" onClick={handleCreatePost}>게시글 등록</button>
+          <button id="logout-icon-btn" onClick={handleLogout}>
+            <img id="logout-icon" src={logout} alt="logout" />
+          </button>
+        </div>
+      </header>
+      <h1 id="site-title-first" className="site-title">
+        뭐 먹고싶어
+      </h1>
+      <h1 id="site-title-second" className="site-title">
+        골라
+      </h1>
+      <Link src="../img/Eat.jpg"></Link>
+      <div id="main-middle-aboutpost">
+        <button className="common-button" onClick={handleCreateFormToggle}>
+          글쓰기
+        </button>
+        <h2>지역별로 고르기</h2>
+        <div className="category-buttons">
+          <button
+            className="region-button"
+            onClick={() => handleCategoryFilter('수도권')}
+          >
+            수도권
+          </button>
+          <button
+            className="region-button"
+            onClick={() => handleCategoryFilter('충청권')}
+          >
+            충청권
+          </button>
+          <button
+            className="region-button"
+            onClick={() => handleCategoryFilter('호남권')}
+          >
+            호남권
+          </button>
+          <button
+            className="region-button"
+            onClick={() => handleCategoryFilter('영남권')}
+          >
+            영남권
+          </button>
+          <button
+            className="region-button"
+            onClick={() => handleCategoryFilter('강원권')}
+          >
+            강원권
+          </button>
+          <button
+            className="region-button"
+            onClick={() => handleCategoryFilter('제주권')}
+          >
+            제주권
+          </button>
+        </div>
+        <button className="common-button" onClick={handleSortToggle}>
+          {sortOrder === 'desc' ? '오래된순' : '최신순'}
+        </button>
       </div>
+      {/* 페이지 네이션 */}
+      <div className="pagination">
+        {Array.from({ length: totalPages }, (_, index) => {
+          if (totalPages <= 5) {
+            return (
+              <button
+                key={index + 1}
+                onClick={() => handlePageChange(index + 1)}
+                disabled={index + 1 === currentPage}
+              >
+                {index + 1}
+              </button>
+            );
+          } else {
+            if (currentPage <= 3) {
+              // 처음 5페이지 보여주기
+              if (index < 5) {
+                return (
+                  <button
+                    key={index + 1}
+                    onClick={() => handlePageChange(index + 1)}
+                    disabled={index + 1 === currentPage}
+                  >
+                    {index + 1}
+                  </button>
+                );
+              } else if (index === 5) {
+                // ... 표시
+                return <span key={index}>...</span>;
+              }
+            } else if (currentPage > totalPages - 5) {
+              // 마지막 5페이지 보여주기
+              if (index >= totalPages - 2) {
+                return (
+                  <button
+                    key={index + 1}
+                    onClick={() => handlePageChange(index + 1)}
+                    disabled={index + 1 === currentPage}
+                  >
+                    {index + 1}
+                  </button>
+                );
+              } else if (index === totalPages - 6) {
+                // ... 표시
+                return <span key={index}>...</span>;
+              }
+            } else {
+              // 현재 페이지와 주변 페이지 보여주기
+              if (
+                index === currentPage - 1 ||
+                index === currentPage ||
+                index === currentPage + 1
+              ) {
+                return (
+                  <button
+                    key={index + 1}
+                    onClick={() => handlePageChange(index + 1)}
+                    disabled={index + 1 === currentPage}
+                  >
+                    {index + 1}
+                  </button>
+                );
+              } else if (
+                index === currentPage - 2 ||
+                index === currentPage + 2
+              ) {
+                // ... 표시
+                return <span key={index}>...</span>;
+              }
+            }
+          }
+          return null;
+        })}
+      </div>
+      <div id="posts-grid-mom">
+        <ul className="posts-grid">
+          {allPosts.map((post) => (
+            <li className="post-card" key={post.postId}>
+              <Link to={`/post/${post.postId}`} className="post-link">
+                {post.imageUrl && (
+                  <img
+                    src={
+                      Array.isArray(post.imageUrl)
+                        ? post.imageUrl[0]
+                        : post.imageUrl
+                    }
+                    alt={post.title}
+                    className="post-image"
+                  />
+                )}
+                <h3>{post.title}</h3>
+                <small>{post.nickname}</small>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {showCreateForm && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={handleCreateFormToggle}>
+              &times;
+            </span>
+            <h2 className="modal-header">맛집 추천하기:냠냠:</h2>
+            <input
+              className="modal-title"
+              type="text"
+              name="title"
+              placeholder="제목"
+              value={newPost.title}
+              onChange={handleInputChange}
+            />
+            <textarea
+              className="modal-content-input"
+              type="text"
+              name="content"
+              placeholder="내용을 작성해주세요"
+              value={newPost.content}
+              onChange={handleInputChange}
+            ></textarea>
+            <input
+              type="file"
+              name="imageUrl"
+              placeholder="Image URL"
+              // value={newPost.imageUrl}
+              onChange={handleImageInputChange}
+              multiple // 다중 파일 선택을 지원하도록 multiple 속성을 추가합니다.
+            />
+            <select
+              name="category"
+              value={newPost.regionId}
+              onChange={handleCategoryChange}
+            >
+              <option value="1">수도권</option>
+              <option value="2">충청권</option>
+              <option value="3">호남권</option>
+              <option value="4">영남권</option>
+              <option value="5">강원권</option>
+              <option value="6">제주권</option>
+            </select>
+            <button className="modal-btn" onClick={handleCreatePost}>
+              게시글 등록
+            </button>
+          </div>
+        </div>
+      )}
     </div>
-  )}
-</div>
-
-);
-}
+  );
+};
 export default MainPage;
-
-
